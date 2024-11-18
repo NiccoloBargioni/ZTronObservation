10ad4bdb6d16787eefdc61b12ce8d7e2df531e42
NickTheFreak97 <niccolo.dellarocca@gmail.com>
2024-11-18 11:11:17 +0100

    Attempted to fix or. .replace unregister

--- Sources/ZTronObservation/DefaultImplementations/Mediator/MSAMediator.swift
+++ Sources/ZTronObservation/DefaultImplementations/Mediator/MSAMediator.swift
@@ -66,6 +66,7 @@ public final class MSAMediator: Mediator, @unchecked Sendable {
                 if let oldComponentWithSameID = oldComponentWithSameID {
                     self.unregister(oldComponentWithSameID)
                 }
+                self.register(component)
             }
         } else {
             if componentExists {
